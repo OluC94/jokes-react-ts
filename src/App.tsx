e206a358f3b37426f14d10cb82022a2ef0062b62
OluC94 <coker.olu94@gmail.com>
2024-07-17 15:44:34 +0100

    data retrieved from live API linked to SQL db

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Joke from "./components/Joke";
-
+import "./App.css";
 
 const App = () => {
-    const [jokeData, setJokeData] = useState<any[]>([]);
+  const [jokeData, setJokeData] = useState<any[]>([]);
 
-    const fetchAndStoreJokes = async () => {
-        const httpResponse = await axios.get("https://jokes-api-v1.onrender.com/jokes")
-        setJokeData(httpResponse.data)
-    }
+  const fetchAndStoreJokes = async () => {
+    const httpResponse = await axios.get(
+      "https://jokes-app-db-connection.onrender.com/jokes"
+    );
+    setJokeData(httpResponse.data);
+  };
 
-    useEffect(() => {
-        fetchAndStoreJokes()
-        console.log("effect ran")
-        
-    }, [])
+  useEffect(() => {
+    fetchAndStoreJokes();
+    console.log("effect ran");
+  }, []);
 
-    const jokeElements = jokeData.map(joke => {
-        return (<Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />)
-    })
-    return (<main>
-        {jokeElements}
-    </main>)
-}
+  const jokeElements = jokeData.map((joke) => {
+    return <Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />;
+  });
+  return (
+    <main>
+      <div className="jokes">{jokeElements}</div>
+    </main>
+  );
+};
 
-export default App
+export default App;
