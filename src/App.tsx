3eaf87eb80809aef7d414a12e65979277209b7b3
OluC94 <coker.olu94@gmail.com>
2024-07-18 11:55:55 +0100

    add posting functionality for new jokes

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,50 @@ import { useEffect, useState } from "react";
 import Joke from "./components/Joke";
 import "./App.css";
 
+interface JokeType {
+  setup: string;
+  punchline: string;
+}
+
+const baseURL = "https://jokes-app-db-connection.onrender.com";
+
 const App = () => {
   const [jokeData, setJokeData] = useState<any[]>([]);
+  const [newJoke, setNewJoke] = useState<JokeType>({
+    setup: "",
+    punchline: "",
+  });
 
-  const fetchAndStoreJokes = async () => {
-    const httpResponse = await axios.get(
-      "https://jokes-app-db-connection.onrender.com/jokes"
-    );
+  const fetchAndStoreJokes = async (): Promise<void> => {
+    const httpResponse = await axios.get(`${baseURL}/jokes`);
     setJokeData(httpResponse.data);
   };
 
-  const handleSubmitJoke = () => {}
+  const saveNewJoke = async (jokeData: JokeType): Promise<void> => {
+    const httpResponse = await axios.post(`${baseURL}/jokes`, jokeData);
+    if (httpResponse.data.outcome === "success") {
+      alert("Success: please refresh to see your new joke");
+      setNewJoke({
+        setup: "",
+        punchline: "",
+      });
+    } else {
+      alert("Something went wrong");
+    }
+  };
 
-  const handleJokeInput = () => {}
+  const handleSubmitJoke = () => {
+    saveNewJoke(newJoke);
+  };
+
+  const handleJokeInput = (e: { target: { value: string; name: string } }) => {
+    setNewJoke((prevJoke) => {
+      return {
+        ...prevJoke,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
 
   useEffect(() => {
     fetchAndStoreJokes();
@@ -27,11 +58,21 @@ const App = () => {
   });
   return (
     <main>
-        <section className="add-joke">
-        <textarea placeholder="Setup (optional)" name="setup" onChange={handleJokeInput}/>
-        <textarea placeholder="Punchline" name="punchline" onChange={handleJokeInput}/>
+      <section className="add-joke">
+        <textarea
+          placeholder="Setup (optional)"
+          name="setup"
+          onChange={handleJokeInput}
+          value={newJoke.setup}
+        />
+        <textarea
+          placeholder="Punchline"
+          name="punchline"
+          onChange={handleJokeInput}
+          value={newJoke.punchline}
+        />
         <button onClick={handleSubmitJoke}>Submit</button>
-        </section>
+      </section>
       <div className="jokes">{jokeElements}</div>
     </main>
   );
